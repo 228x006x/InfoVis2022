c98f79788d4fa7c8de883a23e75c35ad3491c8d7
小山裕也 <koyamayuuya@MacBook-Pro.local>
2022-04-27 14:36:47 +0900

    W06

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -55,7 +55,7 @@ class ScatterPlot {
             .ticks(6);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
+            .attr('transform', `translate(0, ${self.inner_height})`);
         
         self.yaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_width})`);
