204c720cf52e7dc0ec776871a838e065fc19df31
小山裕也 <koyamayuuya@MacBook-Pro.local>
2022-04-27 14:46:15 +0900

    W06

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -6,7 +6,7 @@ d3.csv("https://228x006x.github.io/InfoVis2022/W06/w06_task1_index.html")
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:20, right:20, bottom:20, left:20}
+            margin: {top:10, right:10, bottom:20, left:10}
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -23,7 +23,7 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:20, right:20, bottom:20, left:20}
+            margin: config.margin || {top:10, right:10, bottom:20, left:10}
         }
         this.data = data;
         this.init();
@@ -58,7 +58,7 @@ class ScatterPlot {
             .attr('transform', `translate(0, ${self.inner_height})`);
         
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate( ${self.inner_width}),0`);
+            .attr('transform', `translate( ${self.inner_width},0)`);
         
         
     }
